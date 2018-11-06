ff986f800943957be9a33a8eb0c4993a2aedd549
Nikolay Nozdrin-Plotnitsky <nozdrin.plotnitsky@karma.red>
2018-11-06 14:55:46 +0300

    add timeout fetch

--- kafka_transport/__main__.py
+++ kafka_transport/__main__.py
@@ -1,6 +1,7 @@
 import asyncio
 import msgpack
 import uuid
+import time
 from types import CoroutineType
 from aiokafka import AIOKafkaConsumer, AIOKafkaProducer
 
@@ -8,6 +9,14 @@ loop = asyncio.get_event_loop()
 
 kafka_host = None
 producer = None
+
+class KafkaTransportError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+    
+    def __str__(self):
+        return self.msg
+
 
 def encode_key(key) -> str:
     if key == None:
@@ -18,6 +27,7 @@ def encode_key(key) -> str:
 
     return key.encode('utf8')
 
+
 def decode_key(key) -> str:
     if key == None:
         return None
@@ -27,6 +37,7 @@ def decode_key(key) -> str:
 
     return key.decode('utf8')
 
+
 async def init(host):
     global kafka_host
     global producer
@@ -35,6 +46,7 @@ async def init(host):
     producer = AIOKafkaProducer(
         loop=loop, bootstrap_servers=kafka_host)
     await producer.start()
+
 
 async def subscribe(topic, callback):
     consumer = AIOKafkaConsumer(
@@ -54,11 +66,13 @@ async def subscribe(topic, callback):
 
     # await consumer.stop()
 
+
 async def push(topic, value, key=None):
     data = msgpack.packb(value, use_bin_type=True)
     await producer.send(topic, data, key=encode_key(key))
 
-async def fetch(to, _from, value):
+
+async def fetch(to, _from, value, timeout_ms=600 * 1000, loop=loop):
     id = str(uuid.uuid4())
 
     consumer = AIOKafkaConsumer(
@@ -68,11 +82,19 @@ async def fetch(to, _from, value):
     await push(to, value, id)
     await consumer.start()
 
-    async for msg in consumer:
-        try:
-            key = decode_key(msg.key)
-            if key == id:
-                await consumer.stop()
-                return msgpack.unpackb(msg.value, raw=False)
-        except:
-            print("Not binary data")
+    try:
+        end_time = time.time() + timeout_ms / 1000
+
+        while time.time() <= end_time:
+          result = await consumer.getmany(timeout_ms=timeout_ms)
+          for messages in result.values():
+              for msg in messages:
+                  key = decode_key(msg.key)
+                  if key == id:
+                      await consumer.stop()
+                      return msgpack.unpackb(msg.value, raw=False)
+    finally:
+        await consumer.stop()
+
+    raise KafkaTransportError("Fetch timeout")
+
